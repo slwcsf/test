6f4cc25c2214e91ca12f7d403706a44ec3eb564e
sunlw <748726735@qq.com>
2022-10-11 11:22:16 +0800

    增加事件测试

---
+++ net-java/Myevent/Car.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.sunlw.net.Myevent
+{
+    public class Car
+    {
+        // public delegate string Myd(int speed);
+        public string brand { get; set; }
+
+        public Car(string brand)
+        {
+            this.brand = brand;
+        }
+    }
+
+    public class DriveCar
+    {
+        public int speed;
+        public Car car;
+
+        public DriveCar(Car car)
+        {
+            this.car = car;
+        }
+
+        public event Func<int, string>? speedChangeEvent;
+
+        public event EventHandler<bool> carrypassengerEventHanler;
+
+        //{
+        //    add
+        //    {
+        //        value!.Invoke(this, true);
+        //        Console.WriteLine($"这是个啥");
+        //    }
+        //    remove { }
+        //}
+
+        public void ChangeSpeed(int speed)
+        {
+            Console.WriteLine($"车:我开{car.brand}车的速度是{speed}");
+            if (speedChangeEvent != null)
+            {
+                speedChangeEvent!.Invoke(speed);
+            }
+        }
+
+        public void ChangeCarrypassenger(bool isCarrypassenger)
+        {
+            Console.WriteLine($"车:我开{car.brand}车要拉人了");
+            carrypassengerEventHanler!.Invoke(this, isCarrypassenger);
+        }
+    }
+
+    public class Person
+    {
+        public string name { get; set; }
+        private DriveCar carSpeed;
+
+        public Person(DriveCar carSpeed, string name)
+        {
+            this.carSpeed = carSpeed;
+            this.name = name;
+            //this.carSpeed.speedChangeEvent += Warning;
+        }
+
+        public void NotCareCarSpeed()
+        {
+            Console.WriteLine($"{name}:我不关注车速了");
+            this.carSpeed.speedChangeEvent -= Warning;
+        }
+
+        public void CareCarSpeed()
+        {
+            Console.WriteLine($"{name}:我要关注车速");
+            this.carSpeed.speedChangeEvent += Warning;
+        }
+
+        public string Warning(int speed)
+        {
+            Console.WriteLine($"{name}:这个车的速度是{speed},我要注意了");
+            return $"车的速度是{speed},我要注意了";
+        }
+
+        public void CareCarCarrypassenger()
+        {
+            Console.WriteLine($"{name}:我要关注车是不是拉人了");
+            this.carSpeed.carrypassengerEventHanler += CarCarrypassenger;
+        }
+
+        public void CarCarrypassenger(object? sender, bool e)
+        {
+            Console.WriteLine($"{sender!.ToString()}拉人了?={e}");
+        }
+    }
+
+    public class Person2
+    {
+        public string name { get; set; }
+
+        public Person2(string name)
+        {
+            this.name = name;
+            //this.carSpeed.speedChangeEvent += Warning;
+        }
+
+        public void Warning(object sender, int speed)
+        {
+            Console.WriteLine($"{sender},{name}:这个车的速度是{speed},我要注意了");
+        }
+    }
+
+    public struct CarSpeedWapper<T>
+    {
+        private event EventHandler<T>? _event;
+
+        private Delegate[]? _handlers;
+
+        public void AddHandler(EventHandler<T>? handler)
+        {
+            _event += handler;
+            _handlers = null;
+        }
+
+        public void RemoveHandler(EventHandler<T>? handler)
+        {
+            _event -= handler;
+            _handlers = null;
+        }
+
+        public void Invoke(object sender, T parameter)
+        {
+            var handlers = _handlers;
+            if (handlers is null)
+            {
+                handlers = _event?.GetInvocationList();
+                if (handlers is null)
+                {
+                    return;
+                }
+
+                _handlers = handlers;
+            }
+            foreach (EventHandler<T> action in handlers)
+            {
+                try
+                {
+                    action(sender, parameter);
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+        }
+    }
+
+    public class CarSpeed
+    {
+        public event EventHandler<int> Callback
+        {
+            add => _callbackWrapper.AddHandler(value);
+            remove => _callbackWrapper.RemoveHandler(value);
+        }
+
+        private CarSpeedWapper<int> _callbackWrapper;
+
+        public void Start(object sender, int speed)
+        {
+            _callbackWrapper.Invoke(sender, speed);
+        }
+    }
+
+    public class CarSpeed0
+    {
+        public event EventHandler<int> Callback;
+
+        public void Start(object sender, int speed)
+        {
+            Callback.Invoke(sender, speed);
+        }
+    }
+
+    public struct Book
+    {
+        public string name { get; set; }
+        public string mark { get; set; }
+        public int page { get; set; }
+
+        public Auther auth { get; set; }
+    }
+
+    public struct Auther
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+    }
+}
--- net-java/Program.cs
+++ net-java/Program.cs
@@ -1,15 +1,110 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using com.sunlw.net.Myevent;
 using com.sunlw.net.reflect;
+using System.Runtime.CompilerServices;
+using System.Text;
 
-internal class Program
+Console.WriteLine("Hello, World!");
+
+#region 反射运行测试代码
+
+//反射对象
+ReflectDemo.TestObj();
+//反射方法
+ReflectDemo.TestMethond();
+await ReflectDemo.TestAsyncMethond();
+
+#endregion 反射运行测试代码
+
+#region 事件测试代码
+
+/*
+ *人关注车速,当车速变化会通知所有关注车速的人
+ *人也取消关注车速,车速变化后不会通知该人
+ */
+Car car = new Car("奔驰");
+var driveCar = new DriveCar(car);
+Person person1 = new Person(driveCar, "张三");
+Person person2 = new Person(driveCar, "李四");
+person1.CareCarSpeed();
+person2.CareCarSpeed();
+driveCar.ChangeSpeed(100);
+driveCar.ChangeSpeed(200);
+person1.NotCareCarSpeed();
+driveCar.ChangeSpeed(speed: 300);
+person1.CareCarSpeed();
+driveCar.ChangeSpeed(speed: 400);
+
+person1.CareCarCarrypassenger();
+driveCar.ChangeCarrypassenger(true);
+
+#endregion 事件测试代码
+
+#region 事件测试代码2
+
+CarSpeed0 carSpeed0 = new CarSpeed0();
+carSpeed0.Callback += (obj, a) =>
 {
-    private static async Task Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-        //反射对象
-        ReflectDemo.TestObj();
-        //反射方法
-        ReflectDemo.TestMethond();
-        await ReflectDemo.TestAsyncMethond();
-    }
-}
+    Console.WriteLine($"{obj!.ToString()}我要加速了,{a}");
+};
+Person2 person20 = new Person2("王五");
+carSpeed0.Callback += person20.Warning!;
+
+carSpeed0.Start(new object { }, 500);
+
+#endregion 事件测试代码2
+
+#region 事件包装类
+
+/*
+* 为了扩展用
+*/
+CarSpeed carSpeed = new CarSpeed();
+carSpeed.Callback += (obj, a) =>
+{
+    Console.WriteLine($"{obj!.ToString()}我要加速了,{a}");
+};
+Person2 person21 = new Person2("赵六");
+carSpeed.Callback += person21.Warning!;
+
+carSpeed.Start(new object { }, 200);
+
+#endregion 事件包装类
+
+#region struct
+
+Book book = new();
+book.name = "name";
+var auth = new Auther();
+book.auth = auth;
+auth.name = "张三";
+book.auth = auth;
+Console.WriteLine(book.name);
+Console.WriteLine(book.mark);
+Console.WriteLine(book.page);
+Console.WriteLine(book.auth.name);
+auth.name = "李四";
+Console.WriteLine(book.auth.name);//这个地方是张三,不是李四 struct是值类型
+
+#endregion struct
+
+#region 编码测试
+
+/*
+ * ASCII码是通用的,UTF8等是在基础上进行了增加
+ * A你B9 对应的字节
+ * 65 228 189 160 66 57
+ * 65 ->A
+ * 228 189 160 ->你 (一个汉字3个字节)
+ * 66 ->B
+ * 57 ->9
+ */
+var b_1 = Encoding.UTF8.GetBytes("A你B9");
+var b_2 = Encoding.UTF8.GetString(b_1, 0, b_1.Length);
+var b_3 = Encoding.ASCII.GetString(b_1, 0, b_1.Length);//A???B9 没有的ASCII会解析成?
+var b_4 = (byte)'A'; //可以直接转字节 byte->65
+var b_5 = (byte)125; //byte->125
+
+#endregion 编码测试
+
+Console.ReadLine();
